9575dba53559e656676bb69ca3992a77e4136328
GiftedByYahweh <giftedbyyahweh@gmail.com>
2025-09-26 00:20:42 +0300

    fix

--- src/layouts/default/DefaultLayout.vue
+++ src/layouts/default/DefaultLayout.vue
@@ -1,16 +1,7 @@
 <script setup></script>
 
 <template>
-  <n-layout content-class="default-layout">
-    <router-view></router-view>
+  <n-layout content-style="display: flex; justify-content: center;height: 100vh; align-items: center;">
+    <router-view />
   </n-layout>
 </template>
-
-<style>
-.default-layout {
-  display: flex;
-  justify-content: center;
-  height: 100vh;
-  align-items: center;
-}
-</style>
