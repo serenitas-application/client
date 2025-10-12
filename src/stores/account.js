5a92436580bbbfd2d684d96106c85cbce6d50980
GiftedByYahweh <giftedbyyahweh@gmail.com>
2025-10-12 15:43:34 +0300

    add auth / account route update

--- src/stores/account.js
+++ src/stores/account.js
@@ -1,8 +1,11 @@
-import { defineStore } from "pinia";
-import { computed, ref } from "vue";
+import { defineStore } from 'pinia';
+import { computed, ref } from 'vue';
+import { ACCOUNT_LS } from '../common/consts/local-storage';
 
-export const useAccountStore = defineStore("account", () => {
-  const account = ref(null);
+export const useAccountStore = defineStore('account', () => {
+  const hasAccount = localStorage.getItem(ACCOUNT_LS);
+
+  const account = ref(!!hasAccount);
 
   const isAuth = computed(() => account.value);
 
