5426187d14b83a03619442fdef1266ac637caa53
GiftedByYahweh <giftedbyyahweh@gmail.com>
2025-10-14 23:55:46 +0300

    page update

--- src/modules/diary/DiaryPage.vue
+++ src/modules/diary/DiaryPage.vue
@@ -1,13 +1,5 @@
 <script setup>
 import DiaryForm from './DiaryForm.vue';
-
-// import { useRouter } from 'vue-router';
-// import { RoutePaths } from '../../router/routes';
-
-// const router = useRouter();
-
-// const toCreatePage = () =>
-//   router.push({ name: RoutePaths.pages.children.create.name });
 </script>
 
 <template>
