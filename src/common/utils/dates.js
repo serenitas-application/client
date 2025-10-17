4de5452c020c3779d3db5b41c9f007bbc3b7836f
GiftedByYahweh <giftedbyyahweh@gmail.com>
2025-10-17 23:35:09 +0300

    update dates common

--- src/common/utils/dates.js
+++ src/common/utils/dates.js
@@ -19,3 +19,11 @@ export const formatDateWithWeekday = (date) => {
 
   return `${weekday}, ${formattedDate}`;
 };
+
+export const formatDateToYMD = (ts) => {
+  const date = new Date(ts);
+  const y = date.getFullYear();
+  const m = String(date.getMonth() + 1).padStart(2, '0');
+  const d = String(date.getDate()).padStart(2, '0');
+  return `${y}-${m}-${d}`;
+};
