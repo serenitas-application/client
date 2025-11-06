0accba9643701c1d080fb6b5175b764175aee0be
GiftedByYahweh <giftedbyyahweh@gmail.com>
2025-11-06 07:23:27 +0200

    update account card / update formatDateWithWeekday function

--- src/modules/account/AccountCard.vue
+++ src/modules/account/AccountCard.vue
@@ -1,22 +1,33 @@
 <script setup>
+import { computed } from 'vue';
 import { useAccountStore } from '../../stores/account';
+import { formatDateWithWeekday } from '../../common/utils/dates';
 
 const account = useAccountStore();
+const createAccountDate = computed(() => {
+  return formatDateWithWeekday(account.account.createdDate);
+});
 </script>
 
 <template>
-  <n-card>
-    <div class="flex a-center g-12">
-      <n-p style="font-weight: 600"> Username: </n-p>
-      <n-p>{{ account.account.username }}</n-p>
+  <n-card class="card">
+    <div class="flex j-between i-center g-4">
+      <n-p> Username: </n-p>
+      <n-p>{{ account.account.username ?? '-' }}</n-p>
     </div>
-    <div class="flex a-center g-12">
-      <n-p style="font-weight: 600"> Email: </n-p>
-      <n-p>{{ account.account.email }}</n-p>
+    <div class="flex j-between i-center g-4">
+      <n-p> Email: </n-p>
+      <n-p>{{ account.account.email ?? '-' }}</n-p>
     </div>
-    <div class="flex a-center g-12">
-      <n-p style="font-weight: 600"> Create date: </n-p>
-      <n-p>{{ new Date(account.account.createdDate).toDateString() }}</n-p>
+    <div class="flex j-between i-center g-4">
+      <n-p> Create date: </n-p>
+      <n-p>{{ createAccountDate }}</n-p>
     </div>
   </n-card>
 </template>
+
+<style scoped>
+.card {
+  max-width: 500px;
+}
+</style>
